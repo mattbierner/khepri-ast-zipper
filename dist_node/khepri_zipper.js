16cd999a3a05597b379d6a0483eef7674393f883
Matt Bierner <mattbierner@gmail.com>
2014-01-26 20:40:49 -0500

    Updated node file

--- dist_node/khepri_zipper.js
+++ dist_node/khepri_zipper.js
@@ -1,27 +1,28 @@
-define(["require", "exports", "nu-stream/gen", "nu-stream/stream", "neith/tree", "khepri-ast/node"], (function(require,
-    exports, __o, __o0, __o1, __o2) {
-    "use strict";
-    var range = __o["range"],
-        foldl = __o0["foldl"],
-        from = __o0["from"],
-        NIL = __o0["NIL"],
-        treeZipper = __o1["treeZipper"],
-        Node = __o2["Node"],
-        modify = __o2["modify"],
-        khepriZipper, buildArray = (function(pairs) {
-            return foldl((function(p, __o3) {
-                var key = __o3["key"],
-                    value = __o3["value"];
-                (p[key] = value);
-                return p;
-            }), [], pairs);
-        });
-    (khepriZipper = treeZipper.bind(null, (function(node) {
-        return (node ? (Array.isArray(node) ? range(0, node.length) : from(node.children)) : NIL);
-    }), (function(n, k) {
-        return n[k];
-    }), (function(node, pairs, values) {
-        return ((node instanceof Node) ? modify(node, values(), ({})) : buildArray(pairs));
-    })));
-    (exports.khepriZipper = khepriZipper);
-}));
+"use strict";
+var __o = require("nu-stream")["gen"],
+    range = __o["range"],
+    __o0 = require("nu-stream")["stream"],
+    foldl = __o0["foldl"],
+    from = __o0["from"],
+    NIL = __o0["NIL"],
+    __o1 = require("neith")["tree"],
+    treeZipper = __o1["treeZipper"],
+    __o2 = require("khepri-ast")["node"],
+    Node = __o2["Node"],
+    modify = __o2["modify"],
+    khepriZipper, buildArray = (function(pairs) {
+        return foldl((function(p, __o3) {
+            var key = __o3["key"],
+                value = __o3["value"];
+            (p[key] = value);
+            return p;
+        }), [], pairs);
+    });
+(khepriZipper = treeZipper.bind(null, (function(node) {
+    return (node ? (Array.isArray(node) ? range(0, node.length) : from(node.children)) : NIL);
+}), (function(n, k) {
+    return n[k];
+}), (function(node, pairs, values) {
+    return ((node instanceof Node) ? modify(node, values(), ({})) : buildArray(pairs));
+})));
+(exports.khepriZipper = khepriZipper);
