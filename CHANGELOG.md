498f3a0085addf7171f173a3d3ecf35b768b061a
Matt Bierner <mattbierner@gmail.com>
2014-02-24 19:29:28 -0500

    V1.1.2, added missing dep to package.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # ChangeLog #
 
+## 1.1.2 - Feb 24, 2014
+* Fixed nu-stream not listed in package.json.
+
 ## 1.1.0 - Feb 13, 2014
 * Exported functions used for zipper metadata.
 * Improved reconstruct to also work with nodes that have the ast node interface but
--- dist/khepri_zipper.js
+++ dist/khepri_zipper.js
@@ -6,22 +6,29 @@ define(["require", "exports", "nu-stream/gen", "nu-stream/stream", "neith/tree",
     exports, __o, __o0, __o1, __o2) {
     "use strict";
     var range = __o["range"],
+        filter = __o0["filter"],
         foldl = __o0["foldl"],
         from = __o0["from"],
         NIL = __o0["NIL"],
         treeZipper = __o1["treeZipper"],
         Node = __o2["Node"],
         modify = __o2["modify"],
-        khepriZipper, getChild, getChildren, construct, buildArray = (function(pairs) {
-            return foldl((function(p, __o3) {
-                var key = __o3["key"],
-                    value = __o3["value"];
+        khepriZipper, getChild, getChildren, construct, nonNullChildren = (function(node) {
+            return node.children.filter((function(k) {
+                return (node[k] !== null);
+            }));
+        }),
+        buildArray = (function(pairs) {
+            return foldl((function(p, __o) {
+                var key = __o["key"],
+                    value = __o["value"];
                 (p[key] = value);
                 return p;
             }), [], pairs);
         });
     (getChildren = (function(node) {
-        return (node ? (Array.isArray(node) ? range(0, node.length) : from(node.children)) : NIL);
+        return (node ? (Array.isArray(node) ? range(0, node.length) : from(nonNullChildren(node))) :
+            NIL);
     }));
     (getChild = (function(node, k) {
         return node[k];
