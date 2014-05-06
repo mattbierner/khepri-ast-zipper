a7b2c5b687e25a9e064fdd7dfc061fc5f9e96ae1
Matt Bierner <mattbierner@gmail.com>
2014-05-06 01:13:18 -0400

    Fixed non node type

--- dist/khepri_zipper.js
+++ dist/khepri_zipper.js
@@ -1,4 +1,7 @@
-define(["require", "exports", "nu-stream/gen", "nu-stream/stream", "neith/tree", "khepri-ast/node"], (function(require,
+/*
+ * THIS FILE IS AUTO GENERATED from 'lib/khepri_zipper.kep'
+ * DO NOT EDIT
+*/define(["require", "exports", "nu-stream/gen", "nu-stream/stream", "neith/tree", "khepri-ast/node"], (function(require,
     exports, __o, __o0, __o1, __o2) {
     "use strict";
     var range = __o["range"],
@@ -8,26 +11,29 @@ define(["require", "exports", "nu-stream/gen", "nu-stream/stream", "neith/tree",
         treeZipper = __o1["treeZipper"],
         Node = __o2["Node"],
         modify = __o2["modify"],
-        khepriZipper, getChild, getChildren, construct, buildArray = (function(pairs) {
-            return foldl((function(p, __o) {
-                var key = __o["key"],
-                    value = __o["value"];
-                (p[key] = value);
-                return p;
-            }), [], pairs);
+        khepriZipper, getChild, getChildren, construct, __dot = (function(x, y) {
+            return x[y];
+        }),
+        isObject = (function(x) {
+            var y = (typeof x);
+            return ("object" === y);
         });
     (getChildren = (function(node) {
-        return (node ? (Array.isArray(node) ? range(0, node.length) : from(node.children)) : NIL);
+        return ((node && isObject(node)) ? (Array.isArray(node) ? range(0, node.length) : from(node.children)) :
+            NIL);
     }));
-    (getChild = (function(node, k) {
-        return node[k];
-    }));
+    (getChild = __dot);
     (construct = (function(node, pairs, values) {
-        return (node ? (Array.isArray(node) ? buildArray(pairs) : modify(node, values(), ({}))) : node);
+        return ((node && isObject(node)) ? (Array.isArray(node) ? foldl((function(p, __o3) {
+            var key = __o3["key"],
+                value = __o3["value"];
+            (p[key] = value);
+            return p;
+        }), [], pairs) : modify(node, values(), ({}))) : node);
     }));
     (khepriZipper = treeZipper.bind(null, getChildren, getChild, construct));
-    (exports.khepriZipper = khepriZipper);
-    (exports.getChild = getChild);
-    (exports.getChildren = getChildren);
-    (exports.construct = construct);
+    (exports["khepriZipper"] = khepriZipper);
+    (exports["getChild"] = getChild);
+    (exports["getChildren"] = getChildren);
+    (exports["construct"] = construct);
 }));
